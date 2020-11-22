34c56f7b27dff13618d6d349d23fdfb7e7a75089
fantommohammed <32329994+fantommohammed@users.noreply.github.com>
2020-11-22 12:43:34 +0200

    new thems

--- public/assets/admin/js/scripts/charts/chartist/bar/stacked-bar.js
+++ public/assets/admin/js/scripts/charts/chartist/bar/stacked-bar.js
@@ -3,7 +3,6 @@
     Description: Chartisrt stacked bar chart
     ----------------------------------------------------------------------------------------
     Item Name: Modern Admin - Clean Bootstrap 4 Dashboard HTML Template
-    Version: 1.0
     Author: PIXINVENT
     Author URL: http://www.themeforest.net/user/pixinvent
 ==========================================================================================*/
--- public/assets/admin/js/scripts/charts/chartist/line/holes-in-data.js
+++ public/assets/admin/js/scripts/charts/chartist/line/holes-in-data.js
@@ -3,7 +3,6 @@
     Description: Chartist holes in data line chart
     ----------------------------------------------------------------------------------------
     Item Name: Modern Admin - Clean Bootstrap 4 Dashboard HTML Template
-    Version: 1.0
     Author: PIXINVENT
     Author URL: http://www.themeforest.net/user/pixinvent
 ==========================================================================================*/
